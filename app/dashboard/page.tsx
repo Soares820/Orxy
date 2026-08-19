47959b3ea54b21fb4cf5ca5fc3a2d8cba5dfeb29
Soares820 <arthur.cleber@hotmail.com>
2026-08-18 23:04:28 -0300

    Revert "feat: adicionar Tawk.to chat de suporte no dashboard"
    This reverts commit 0315ebb986e38fd6bd1131f5e20f45fdc665c404.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,7 +6,6 @@ import Image from 'next/image';
 import { AppProvider, useApp } from '@/contexts/AppContext';
 import type { Screen } from '@/lib/types';
 
-import SupportChat from '@/components/ui/SupportChat';
 import DashboardHome from '@/components/dashboard/screens/DashboardHome';
 import PacientesScreen from '@/components/dashboard/screens/PacientesScreen';
 import AgendaScreen from '@/components/dashboard/screens/AgendaScreen';
@@ -238,7 +237,6 @@ function AppShell() {
           {SCREENS[screen]}
         </div>
       </div>
-      <SupportChat />
     </div>
   );
 }
